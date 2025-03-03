5f15194881635475727a34b8f44c21f0a119ccea
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2025-03-03 08:08:24 -0600

    updated home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import Header from '../../components/Header/Header'
-import { Outlet } from 'react-router-dom'
+
 import About from '../About/About'
-import Projects from '../Projects/Project'
 import Contact from '../Contact/Contact'
 import Herosection from '../../components/Herosection/Herosection'
 import Services from '../Services/Services'
-import Skills from '../Skills/Skills'
+import Project from '../Projects/Project'
 
 const Home = () => {
   return (
     <div>
         <Herosection />
         <About />
-        <Projects />
+        <Project />
         <Services />
         <Contact />
         
